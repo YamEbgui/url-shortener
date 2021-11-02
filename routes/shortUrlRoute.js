caadaefed666bed47c36457aaaa5b4171ff0bb82
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-02 20:48:53 +0200

    Create js file to  router for short Url

---
+++ routes/shortUrlRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const shortUrlRouter = express.Router();
+const fs = require ("fs")
+const path = require ("path");
+
+shortUrlRouter.get("/" ,(req , res) =>{
+
+})
+
+module.exports = shortUrlRouter;
