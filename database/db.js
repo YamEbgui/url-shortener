1eaacc214257ea725f2373b0fff6a2a769cdddc2
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-02 20:44:51 +0200

    Finish define class DataBase

--- database/db.js
+++ database/db.js
@@ -1,70 +1,69 @@
-const fs = require("fs")
-const path = require("path")
-const util = require("util")
-const readFile = (filename) => util.promisify(fs.readFile)(filename , "utf-8")
-class DataBase{
-    static #createShortCut() {
-        const created = true;
-        let shortUrl = ""
-        for (let i=0; i<7; i++) {
-            if (Math.random()<0.5) {
-                shortUrl+= String.fromCharCode(65 + Math.floor(Math.random()*26))
-            }
-            else {
-                shortUrl+= String.fromCharCode(48 + Math.floor(Math.random()*10))
-            }
-        }
-        return shortUrl;
+const fs = require("fs");
+const fsAsync = require("fs/promises");
+const path = require("path");
+const util = require("util");
+const readFile = (filename) => util.promisify(fs.readFile)(filename, "utf-8");
+
+class DataBase {
+  static #createShortCut() {
+    const created = true;
+    let shortUrl = "";
+    for (let i = 0; i < 7; i++) {
+      if (Math.random() < 0.5) {
+        shortUrl += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      } else {
+        shortUrl += String.fromCharCode(48 + Math.floor(Math.random() * 10));
+      }
     }
-    static async #readDataBase() {
-        try {
-            const fileData = await readFile("../db.json")
-            return await fileData
-        }
-        catch(error) {
-            console.error(error)
-        }
-    }
-    static async createUrlObj(_originUrl) {
-        let newShortCut = await this.#createShortCut();
-        while(await this.checkIfUrlExist(newShortCut)){
-            console.log("zain")
-            newShortCut = await this.#createShortCut();
-        }
-        const urlObj = {
-            originUrl: _originUrl,
-            shortUrl: newShortCut,
-            views: 0,
-        }
-        return urlObj;
-    }
-    static async writeUrl() {
-        try {
-            const dataBase = await this.readDataBase();
-            const objectsArr=  await JSON.parse(dataBase).objects;
-            objectsArr.push({"tim" : "tam"});
-            dataBase.objects=objectsArr;
-            fs.writeFile("../db.json", dataBase);
-            return objectsArr;
-            } catch (error) {
-                console.log(error);
-            }
-    }
-    static async checkIfUrlExist(randomSequence){
+    return shortUrl;
+  }
+
+  static async #readDataBase() {
     try {
-            const dataBase = await this.#readDataBase();
-            console.log(dataBase)
-        for (let obj in dataBase.objects) {
-            if (obj.shortUrl === randomSequence) {
-                return true;
-            }
-        }
-        return false;
+      const fileData = await readFile("../db.json");
+      return await fileData;
+    } catch (error) {
+      console.error(error);
     }
-    catch(err) {
-        console.error(err)
+  }
+
+  static async #createUrlObj(_originUrl) {
+    let newShortCut = await this.#createShortCut();
+    while (await this.#checkIfUrlExist(newShortCut)) {
+      newShortCut = await this.#createShortCut();
     }
+    const urlObj = {
+      originUrl: _originUrl,
+      shortUrl: newShortCut,
+      views: 0,
+    };
+    return urlObj;
+  }
+
+  static async addObjToDb(originUrl) {
+    await this.#writeUrl(await this.#createUrlObj(originUrl));
+    console.log("written succesfully");
+  }
+
+  static async #writeUrl(newObj) {
+    const dataBase = JSON.parse(await this.#readDataBase());
+    const objectsArr = dataBase.objects;
+    objectsArr.push(newObj);
+    dataBase.objects = objectsArr;
+    await fsAsync.writeFile("../db.json", JSON.stringify(dataBase));
+  }
+
+  static async #checkIfUrlExist(randomSequence) {
+    const dataBase = await this.#readDataBase();
+    for (let obj in dataBase.objects) {
+      if (obj.shortUrl === randomSequence) {
+        return true;
+      }
     }
+    return false;
+  }
 }
-DataBase.createUrlObj("SSSSSSSSSSSSSSS")
-.then((data) => console.log(data))
+
+DataBase.addObjToDb(
+  "https://github.com/zivserphos/cyber4s-final1-boilerplate-url-shortener/tree/development"
+);
