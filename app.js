691e4ada487c2ec84852487be5863ba416a2019e
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-03 11:04:34 +0200

    Update app to route requests

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const shortUrlRouter = require("./routes/shortUrlRoute");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+app.use(express.json());
 app.use("/public", express.static(`./public`));
+app.use("/api/shorturl/", shortUrlRouter);
+app.use("/api/statistic/", shortUrlRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/", (req, res) => {
+  res.send("test");
 });
 
 module.exports = app;
