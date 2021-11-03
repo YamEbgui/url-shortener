b67cd1e43b5a7422fcf14efa50170d82a277ce48
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-03 16:57:30 +0200

    Add webpack to the project and arrange the files of the project

--- server/database/db.js
+++ server/database/db.js
@@ -3,6 +3,8 @@ const fsAsync = require("fs/promises");
 const path = require("path");
 const util = require("util");
 const readFile = (filename) => util.promisify(fs.readFile)(filename, "utf-8");
+const moment = require("moment");
+
 class DataBase {
   static #createShortCut() {
     const created = true;
@@ -16,14 +18,16 @@ class DataBase {
     }
     return shortUrl;
   }
+
   static async #readDataBase() {
     try {
-      const fileData = await readFile("./db.json");
+      const fileData = await readFile("./server/db.json");
       return fileData;
     } catch (error) {
       console.error(error);
     }
   }
+
   static async #createUrlObj(_originUrl) {
     let newShortCut = await this.#createShortCut();
     while (await this.#checkIfUrlExist(newShortCut)) {
@@ -33,9 +37,11 @@ class DataBase {
       originUrl: _originUrl,
       shortUrl: newShortCut,
       views: 0,
+      creatorDate: moment().format("dddd,MMMM Do YYYY, h:mm:ss a"),
     };
     return urlObj;
   }
+
   static async addObjToDb(originUrl) {
     if (await this.#isShortenExist(originUrl)) {
       return await this.#getShortUrl(originUrl);
@@ -47,7 +53,7 @@ class DataBase {
     const objectsArr = dataBase.objects;
     objectsArr.push(newObj);
     dataBase.objects = objectsArr;
-    await fsAsync.writeFile("./db.json", JSON.stringify(dataBase));
+    await fsAsync.writeFile("./server/db.json", JSON.stringify(dataBase));
     console.log(newObj);
     return newObj.shortUrl;
   }
@@ -81,6 +87,19 @@ class DataBase {
         return dataBase.objects[i].shortUrl;
       }
     }
+  }
+
+  static async getOriginUrl(_shortUrl) {
+    let dataBase = await this.#readDataBase();
+    dataBase = JSON.parse(dataBase);
+    for (let i = 0; i < dataBase.objects.length; i++) {
+      if (dataBase.objects[i].shortUrl === _shortUrl) {
+        dataBase.objects[i].views++;
+        await fsAsync.writeFile("./server/db.json", JSON.stringify(dataBase));
+        return dataBase.objects[i].originUrl;
+      }
+    }
+    return false;
   }
 }
 module.exports = DataBase;
