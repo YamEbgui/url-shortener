e4a67d2429d96305ab397dde883d34580cd82c54
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-03 11:02:45 +0200

    Add statistics router to the routers directory

--- routes/statsRoute.js
+++ routes/statsRoute.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const shortUrlRouter = express.Router();
+const cors = require("cors");
+const statsRouter = express.Router();
 const fs = require("fs");
 const path = require("path");
 const db = require("../database/db");
 
-shortUrlRouter.post("/", async (req, res) => {
-  return res.send(await db.addObjToDb(req.body.originUrl));
-});
+statsRouter.get("/:shorturl-id", (req, res) => {});
 
 module.exports = shortUrlRouter;
