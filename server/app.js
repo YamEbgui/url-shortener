6155a1a7497ad7bbc71564b9c832aede9a33367a
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-05 02:12:41 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use("/", reDirectRouter);
-app.use("/public", express.static(`./public`));
 app.use("/api/shorturl/", shortUrlRouter);
 app.use("/api/stats/", statsUrlRouter);
+app.use("/", express.static(path.resolve(`./dist`)));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./dist/index.html"));
 });
